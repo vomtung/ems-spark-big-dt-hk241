c2e1955f7b1485d5c006e4b2ef69371002bb475b
TungVo <tonyvohcmut@gmail.com>
2024-10-28 16:50:08 +0700

    id-2: rest api

---
+++ src/main/java/com/bigdatahk241/servlet/HelloRestApiServlet.java
@@ -0,0 +1,23 @@
+package com.bigdatahk241.servlet;
+
+
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloRestApiServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().write("{\"message\": \"Hello, World!\"}");
+    }
+}
